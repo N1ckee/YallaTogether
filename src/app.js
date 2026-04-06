b934a3a4203fb1f248def94636e835e5233773ef
N1ckee <nicke.hylander@gmail.com>
2026-04-06 16:19:24 +0200

    Redoing the filestructure
    Current filestructure isnt good, updating it to be more in line with
    what vercel wants

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import usersRoutes from "./routes/users.routes.js";
+import { notFound } from "./middleware/notFound.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", usersRoutes);
+
+app.get("/api/health", (req, res) => {
+  res.json({ ok: true });
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
