b7826d92bb9f20148290f734bec59ce73d167c31
N1ckee <nicke.hylander@gmail.com>
2026-04-12 22:15:34 +0200

    Fix

--- src/app.js
+++ src/app.js
@@ -1,25 +1,15 @@
-console.log("APP.JS LOADED");
-
-
 import express from "express";
-// import authRoutes from "./routes/auth.routes.js";
-// import usersRoutes from "./routes/users.routes.js";
-// import { notFound } from "./middleware/notFound.js";
-// import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 
-// app.use("/api/auth", authRoutes);
-// app.use("/api/users", usersRoutes);
+app.get("/", (req, res) => {
+  res.json({ ok: true, message: "API root works" });
+});
 
 app.get("/health", (req, res) => {
   res.json({ ok: true });
 });
-
-// app.use(notFound);
-// app.use(errorHandler);
 
 export default app;
