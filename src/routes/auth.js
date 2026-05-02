d079ce3cdc13556785a001df309063fc02d70c0d
N1ckee <nicke.hylander@gmail.com>
2026-05-02 13:16:21 +0200

    Routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import bcrypt from "bcrypt";
-import pool from "../db/client.js";
+import { pool } from "../db/client.js";
 import jwt from "jsonwebtoken";
 
 const router = Router();
