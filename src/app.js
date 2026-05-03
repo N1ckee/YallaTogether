8001efc8c2a691870e0b92a9dcc27b3cc240af7f
N1ckee <nicke.hylander@gmail.com>
2026-05-03 23:20:26 +0200

    Dashboard

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get("/health", (req, res) => {
